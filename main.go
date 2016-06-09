13bade513dd1507d3a527033be7575cc8878e555
Runi <lnthach2110@gmail.com>
2016-06-09 23:26:05 +0700

    First test again

--- main.go
+++ main.go
@@ -3,20 +3,5 @@ package main
 // WordWrap seperate paragraph with given length by
 // adding \n to end of line
 func WordWrap(para string, length int) string {
-	var result string
-	var j int
-	for i := 0; i <= len(para)-1; i++ {
-		if (j+1)%length == 0 {
-			result += string(para[i]) + "\n"
-		} else if string(para[i]) == " " {
-			result += "\n"
-			j = 0
-			continue
-		} else {
-			result += string(para[i])
-		}
-		j++
-	}
-
-	return result
+	return ""
 }
--- main_test.go
+++ main_test.go
@@ -2,27 +2,10 @@ package main
 
 import "testing"
 
-func TestWordWrapWithLength1(t *testing.T) {
-	w := WordWrap("abc", 1)
-
-	if w != "a\nb\nc\n" {
-		t.Errorf("Expected %q but got %q", "a\nb\nc\n", w)
+func TestWordWrapWithEmptyString(t *testing.T) {
+	w := WordWrap("", 0)
+	if w != "" {
+		t.Errorf("Expected %q but got %q", "", w)
 	}
 
-}
-
-func TestWordWrapWithLengthShorterThanWord(t *testing.T) {
-	w := WordWrap("hello", 3)
-
-	if w != "hel\nlo" {
-		t.Errorf("Expected %q but got %q", "hel\nlo", w)
-	}
-}
-
-func TestWordWrapWithMoreThanTwoWord(t *testing.T) {
-	w := WordWrap("helloa june", 4)
-
-	if w != "hell\noa\njune\n" {
-		t.Errorf("Expected %q but got %q", "hell\noa\njune\n", w)
-	}
 }
