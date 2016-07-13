d7294d7665c718740a5d9eeb067a84ddda061617
Runi <lnthach2110@gmail.com>
2016-07-13 13:09:45 +0700

    Add lower than one is not prime number

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
-import "strings"
-
-// WordWrap seperate paragraph with given length by
-// adding \n to end of line
-func WordWrap(para string, length int) string {
-	if len(para) < length {
-		return para
+func IsPrimeNumber(n int) bool {
+	if n < 1 {
+		return false
 	}
 
-	listWords := strings.Fields(para)
-	var listWordsSanitized []string
-	for _, v := range listWords {
-		if len(v) <= length {
-			listWordsSanitized = append(listWordsSanitized, v)
-		} else {
-			listWordsSanitized = append(listWordsSanitized, v[0:length], v[length:len(v)])
-		}
-
-	}
-	return strings.Join(listWordsSanitized, "\n")
+	return true
 }
--- main_test.go
+++ main_test.go
@@ -2,31 +2,9 @@ package main
 
 import "testing"
 
-func TestWordWrapWithEmptyString(t *testing.T) {
-	w := WordWrap("", 0)
-	if w != "" {
-		t.Errorf("Expected %q but got %q", "", w)
-	}
-
-}
-
-func TestWordWrapWithShorterString(t *testing.T) {
-	w := WordWrap("a", 5)
-	if w != "a" {
-		t.Errorf("Expected %q but got %q", "a", w)
-	}
-}
-
-func TestWordWrapWithWordSperatedEqualToLength(t *testing.T) {
-	w := WordWrap("abc abc abc", 3)
-	if w != "abc\nabc\nabc" {
-		t.Errorf("Expected %q but got %q", "abc\nabc\nabc", w)
-	}
-}
-
-func TestWordWrapWithLengthBiggerThanWordLength(t *testing.T) {
-	w := WordWrap("abcd", 2)
-	if w != "ab\ncd" {
-		t.Errorf("Expected %q but got %q", "ab\ncd", w)
+func TestLowerThanOneIsNotPrimeNumber(t *testing.T) {
+	p := IsPrimeNumber(0)
+	if p {
+		t.Errorf("Expected %q but got %q", false, p)
 	}
 }
